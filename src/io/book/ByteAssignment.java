dd431a83a9800af282f455587028bb32e6711b5e
Jon Poulton <jsp1611@gmail.com>
2013-07-22 19:42:25 +0100

    Added a bit more code

---
+++ src/io/book/ByteAssignment.java
@@ -0,0 +1,19 @@
+package io.book;
+
+/**
+ * @author jon poulton
+ */
+public class ByteAssignment {
+
+    private static byte first = (byte)127;
+
+    private static byte second = 0;
+
+    private static byte third = (byte)300;
+
+    public static void main(String[] args) {
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+    }
+}
